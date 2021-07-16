cbf858215b909ead9a1c194f7aa85c6ca5d292d4
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-07-16 20:33:23 +0530

    Update play.py

--- VCsMusicBot/modules/play.py
+++ VCsMusicBot/modules/play.py
@@ -1088,7 +1088,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+                        "\n\nOr manually add @Tgxbotz_Music_assistant to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
